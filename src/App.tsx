2bfd519ae7d4b0329c6eec45bbf2a2dc2b7a3d7f
jihyundev <aawlgus12@gmail.com>
2024-11-16 17:39:30 +0900

    feat: 토스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes } from "./pages/Routes.tsx";
 import { Providers } from "@/components/providers";
+import { Toaster } from "@/components/ui/Toaster.tsx";
 
 function App() {
   return (
     <Providers>
       <Routes />
+      <Toaster />
     </Providers>
   );
 }
