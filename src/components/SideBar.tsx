80a64781a5fc9715a5489c4ffb8f458f766835e1
jihyundev <aawlgus12@gmail.com>
2024-11-24 21:01:04 +0900

    feat: 홈 버튼 추가

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { Logo } from "@/components/Logo.tsx";
 
 export const SideBar = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-1/3 md:w-[310px] h-screen">
-      <div className="flex items-center space-x-2 w-full h-[60px] border-b mb-2">
+      <button
+        className="flex items-center space-x-2 w-full h-[60px] border-b mb-2"
+        onClick={() => navigate("/")}
+      >
         <Logo width={14} height={20} />
         <span className="text-base text-gray-600 font-semibold flex items-center">
           ClipWise
         </span>
-      </div>
+      </button>
       <div>{children}</div>
     </div>
   );
