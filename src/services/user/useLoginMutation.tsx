bf52845d7b1bf6cebe420b2a8caa4cc0763ce959
jihyundev <aawlgus12@gmail.com>
2024-11-24 16:28:28 +0900

    feat: 로그인 시 쿼리 무효화 설정

--- src/services/user/useLoginMutation.tsx
+++ src/services/user/useLoginMutation.tsx
@@ -1,8 +1,13 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { UserApi } from "@/services/user/userApi";
 
 export const useLoginMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: UserApi.postSocialLogin,
+    onSuccess: () => {
+      queryClient.invalidateQueries();
+    },
   });
 };
